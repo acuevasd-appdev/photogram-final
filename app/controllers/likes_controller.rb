a74c4c2cfed6784f2065cb4593a6270e38cc606c
acuevasd <acuevasd@chicagobooth.edu>
2022-08-13 00:06:54 +0000

    setenta y sies

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -51,6 +51,6 @@ class LikesController < ApplicationController
 
     the_like.destroy
 
-    redirect_to("/likes", { :notice => "Like deleted successfully."} )
+    redirect_to("/likes", { :alert => "Like deleted successfully."} )
   end
 end
